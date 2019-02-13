8939950a81b38a6c9506ff500d36d49f7f38b75c
davidmdm <desmarais.michaud.david@gmail.com>
2019-02-13 11:29:23 -0500

    added nodejs-postgres to lvl-docker-build

--- lvl-docker-build.sh
+++ lvl-docker-build.sh
@@ -48,7 +48,7 @@ export DOCKER_ORG=lvlstudio
 # popd
 
 ## newman depends on nodejs, so order is important
-BUILDERS="nodejs nodejs-mongodb nodejs-mysql nodejs-elasticsearch awscli"
+BUILDERS="nodejs nodejs-mongodb nodejs-mysql nodejs-elasticsearch nodejs-postgres awscli"
 
 ## now loop through the above array
 for i in $BUILDERS
